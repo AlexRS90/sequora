4b61030a08b0f8a1f8768d3e523d203b96a7aace
AlexRS90 <aramosbengy@gmail.com>
2022-04-13 13:39:05 -0500

    Add completet_at column

--- db/migrate/20220413175912_create_orders.rb
+++ db/migrate/20220413175912_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
       t.references :merchant, null: false, foreign_key: true
       t.references :shopper, null: false, foreign_key: true
       t.decimal :amount
+      t.datetime :completed_at
 
       t.timestamps
     end
