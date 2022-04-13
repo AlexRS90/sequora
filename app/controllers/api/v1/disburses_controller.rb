f378476c76869b40dfb979e106536c8dcd3946bb
AlexRS90 <aramosbengy@gmail.com>
2022-04-13 14:34:00 -0500

    Add endpoints to index and show method

--- app/controllers/api/v1/disburses_controller.rb
+++ app/controllers/api/v1/disburses_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::DisbursesController < ApplicationController
   def index
-    render json: { hola: 'pumas' }
+    week = params[:week_num]
+    merchants = Merchant.all
+    render json: { status: 'success', data: merchants }, status: 200
   end
 
   def show
+    merchant = Merchant.where(id: params[:merchant_id])
+    if merchant.any?
+      render json: { statu: 'success', data: merchant }, status: 200
+    else
+      render json: {}, status: :not_found
+    end
   end
 end
